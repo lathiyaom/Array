4dd48585946784b43375e4738cd73ac59bae3efc
om lathiya <lathiya52725@gmail.com>
2024-07-22 10:32:56 +0530

    this is chang

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        int arr[]={1,2,3,4};
+        System.out.println(Arrays.toString(arr));
     }
 }
