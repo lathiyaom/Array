f6a8dc56157e3738e4596edd561fcd6d1f08fa80
om lathiya <lathiya52725@gmail.com>
2024-07-22 10:33:51 +0530

    this is chang

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         int arr[]={1,2,3,4};
+        int []arr1={6,7,8,9};
         System.out.println(Arrays.toString(arr));
     }
 }
